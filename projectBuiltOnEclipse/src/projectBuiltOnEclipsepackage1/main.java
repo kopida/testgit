666070c0031a9f00d760f93a96aee8051eac7e7c
Chau Nguyen <m.chautn2006@gmail.com>
2021-07-03 12:56:01 +1000

    add customer class

--- projectBuiltOnEclipse/src/projectBuiltOnEclipsepackage1/main.java
+++ projectBuiltOnEclipse/src/projectBuiltOnEclipsepackage1/main.java
@@ -5,6 +5,15 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("main class");
+		
+		customer customerA = new customer();
+		customerA.setName("chau");
+		customerA.setPhoneNumber("0916176196");
+		System.out.print(customerA.getName());
+		System.out.print(customerA.getPhoneNumber());
+		
+		
+		
 
 	}
 
