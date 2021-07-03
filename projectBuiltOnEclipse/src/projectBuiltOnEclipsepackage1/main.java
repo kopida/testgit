6d6d4f4d9630d5bc918f5f7626eae48095d82c08
Chau Nguyen <m.chautn2006@gmail.com>
2021-07-03 12:35:10 +1000

    main class

---
+++ projectBuiltOnEclipse/src/projectBuiltOnEclipsepackage1/main.java
@@ -0,0 +1,11 @@
+package projectBuiltOnEclipsepackage1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("main class");
+
+	}
+
+}
---
+++ projectBuiltOnEclipse/src/projectBuiltOnEclipsepackage1/package-info.java
@@ -0,0 +1,1 @@
+package projectBuiltOnEclipsepackage1;
